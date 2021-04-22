3a2f6f4d03d249557f0d402dad6c3aaa7cb6a48c
Clement Ansel <clem-nogare@hotmail.fr>
2021-04-22 11:17:33 +0200

    ajout du fichier de config, du data.sql ainsi que des fichiers static

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (firstname, lastname, mail, password, wallet) VALUES('clément',
+ 'ansel', 'ansel@test', '$2y$10$nbyiHW9ccHCRSaJETH3bCOsM5mv25BEcgO7wkVgKub1gedbtJea0O', 0);
+
+INSERT INTO users (firstname, lastname, mail, password, wallet) VALUES('claire',
+  'bodhuin', 'bodhuin@test', '$2y$10$hTIzf3oYWp5M8W.Iuv9p2.Yt5iyp1fE6Mhaz9dYALaumJVwDSPREe', 0);
+
